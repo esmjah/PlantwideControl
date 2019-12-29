b3cf9ee968352bae519e2dea28d35e64f268ccbe
esmjahan <esmaeil.jahanshahi@hotmail.com>
2019-12-29 22:14:14 +0100

    Changed the prediction horizon to 10 hours.

--- SavedResults/SaveSimData_NMPC_olga.py
+++ SavedResults/SaveSimData_NMPC_olga.py
@@ -116,5 +116,5 @@ simData['yOptimal'] = yOptimal
 simData['J_NMPC'] = J
 simData['Ju_NMPC'] = Ju_NMPC
 simData['simTime'] = sim_time
-simData['compTime'] = computation_time/3600
-sio.savemat('SavedResults\\Nominal_NMPC_600_10.mat', simData)
+
+sio.savemat('SavedResults\\D3D4_NMPC_long.mat', simData)

36e5af2c4bc3a44fa226111ecd2994ac5199fa22
esmjah <esmaeil.jahanshahi@hotmail.com>
2019-12-30 23:55:11 +0100

    Changed prediction horizon to 8 hours.

--- olgaModels/NetworkOPC/Network_Nominal.bat
+++ olgaModels/NetworkOPC/Network_Nominal.bat
@@ -1,6 +1,6 @@
 @echo off
 title "Running Network_Nominal.genkey with C:\Program Files (x86)\Schlumberger\OLGA 2014.1.0\OlgaExecutables\OLGA-2014.1.0.exe"
-pushd "C:\Net2014.1"
+pushd "C:\Git\PlantwideControl\olgaModels\NetworkOPC"
 call "C:\Program Files (x86)\Schlumberger\OLGA 2014.1.0\OlgaExecutables\OLGA-2014.1.0.exe" "Network_Nominal.genkey"
 title "Finished Network_Nominal.genkey"
 popd
